2ae97da6f417663a5ab09b28631abea152304462
Daniel Sousa <daniel.sousa@theintentmedia.com>
2025-05-12 22:08:51 +0100

    .

--- storefront/src/modules/categories/templates/index.tsx
+++ storefront/src/modules/categories/templates/index.tsx
@@ -57,7 +57,7 @@ export default function CategoryTemplate({
           </div>
         )}
         {category.category_children && (
-          <div className="mb-8 text-base-large">
+          <div className="mb-8 text-base-large hidden">
             <ul className=" grid-cols-1 gap-2 hidden">
               {category.category_children?.map((c) => (
                 <li key={c.id}>
