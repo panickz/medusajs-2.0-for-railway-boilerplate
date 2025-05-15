65ffc785f6bc0aa071cf654e01b674db1924786c
Daniel Sousa <daniel.sousa@theintentmedia.com>
2025-05-15 19:41:38 +0100

    .

--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -31,8 +31,8 @@ export default async function Nav() {
             </LocalizedClientLink>
           </div>
 
-          <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
-            <div className="hidden small:flex items-center gap-x-6 h-full">
+          <div className="flex items-center gap-x-4 h-full flex-1 basis-0 justify-end">
+            <div className="small:flex items-center gap-x-6 h-full">
               {process.env.NEXT_PUBLIC_FEATURE_SEARCH_ENABLED && (
                 <LocalizedClientLink
                   className="hover:text-ui-fg-base"
@@ -44,7 +44,7 @@ export default async function Nav() {
                 </LocalizedClientLink>
               )}
               <LocalizedClientLink
-                className="hover:text-ui-fg-base"
+                className="hover:text-ui-fg-base h-full flex items-center"
                 href="/account"
                 aria-label="Account"
                 data-testid="nav-account-link"
